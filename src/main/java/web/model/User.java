e11d330750c40e6181b78a384e76bb3e0e2155e9
AlexKanivets <al.kanivets@mail.ru>
2021-12-17 23:57:32 +0300

    fix

--- src/main/java/web/model/User.java
+++ src/main/java/web/model/User.java
@@ -28,7 +28,7 @@ public class User implements UserDetails {
     @Column(name = "password")
     private String password;
 
-    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
